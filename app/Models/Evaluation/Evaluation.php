30c61cc79d0dc070e95181a1ba596aef16a463ee
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-08-24 18:17:05 -0300

    Evaluation - Model and Migration

---
+++ app/Models/Evaluation/Evaluation.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Evaluation;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Evaluation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 'company' , 'stars', 'comment' ];
+
+} // Evaluation
