24b4954b124e326a7a56ccd14afb9239bd3c7491
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-09-07 11:50:26 -0300

    Configuration - Test

---
+++ database/factories/Api/V1/Evaluation/EvaluationFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories\Api\V1\Evaluation;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Api\V1\Evaluation\Evaluation;
+
+use Illuminate\Support\Str;
+
+class EvaluationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Evaluation::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+
+            'company'       => (string) Str::uuid(),
+            'stars'         => $this->faker->sentence( 10 ),
+            'comment'       => 5
+
+        ]; // return
+
+    } // definition
+} // EvaluationFactory
